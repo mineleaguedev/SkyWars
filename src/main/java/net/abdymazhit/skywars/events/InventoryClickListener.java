ae55d7430d7621270ceaa7c8b066d4f86c71467f
Abdymazhit <islam.abdymazhit@gmail.com>
2022-01-30 21:14:53 +0300

    Добавлена возможность зачаровывать предмет по клику

--- src/main/java/net/abdymazhit/skywars/events/InventoryClickListener.java
+++ src/main/java/net/abdymazhit/skywars/events/InventoryClickListener.java
@@ -3,11 +3,15 @@ package net.abdymazhit.skywars.events;
 import net.abdymazhit.skywars.SkyWars;
 import net.abdymazhit.skywars.enums.GameState;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.EnchantingInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 /**
  * Событие клика по слоту инвентаря
@@ -28,20 +32,61 @@ public class InventoryClickListener implements Listener {
                 return;
             }
             SkyWars.getGameItemsManager().clickInventory(player, event.getInventory(), event.getSlot());
-        }
+        } else {
+            onClickWithEnchantmentBook(event);
 
-        if(SkyWars.getGameManager().getSpectators().contains(player)) {
-            event.setCancelled(true);
-            if(event.getCurrentItem() == null || event.getCurrentItem().getType().equals(Material.AIR)) {
-                return;
+            if(SkyWars.getGameManager().getSpectators().contains(player)) {
+                event.setCancelled(true);
+                if(event.getCurrentItem() == null || event.getCurrentItem().getType().equals(Material.AIR)) {
+                    return;
+                }
+                SkyWars.getGameItemsManager().clickInventory(player, event.getInventory(), event.getSlot());
             }
-            SkyWars.getGameItemsManager().clickInventory(player, event.getInventory(), event.getSlot());
+
+            // Проверка, пытается ли игрок забрать лазурит с стола зачарований
+            if (event.getClickedInventory() instanceof EnchantingInventory && event.getSlot() == 1) {
+                // Отменить клик, так как игрок пытается забрать себе лазурит
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    /**
+     * Событие клика по предмету книгой зачарования
+     * @param event Событие клика слота инвентаря
+     */
+    private void onClickWithEnchantmentBook(InventoryClickEvent event) {
+        if(!event.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)) {
+            return;
         }
 
-        // Проверка, пытается ли игрок забрать лазурит с стола зачарований
-        if (event.getClickedInventory() instanceof EnchantingInventory && event.getSlot() == 1) {
-            // Отменить клик, так как игрок пытается забрать себе лазурит
-            event.setCancelled(true);
+        ItemStack cursorItem = event.getCursor();
+        ItemStack currentItem = event.getCurrentItem();
+
+        if(!cursorItem.getType().equals(Material.ENCHANTED_BOOK) || currentItem.getType().equals(Material.AIR)) {
+            return;
+        }
+
+        if(!(cursorItem.getItemMeta() instanceof EnchantmentStorageMeta)) {
+            return;
+        }
+
+        EnchantmentStorageMeta enchantmentStorageMeta = (EnchantmentStorageMeta) cursorItem.getItemMeta();
+        for(Enchantment enchantment : enchantmentStorageMeta.getStoredEnchants().keySet()) {
+            if(currentItem.getItemMeta().hasConflictingEnchant(enchantment)) {
+                continue;
+            }
+
+            if(!enchantment.canEnchantItem(currentItem)) {
+                continue;
+            }
+
+            currentItem.addEnchantment(enchantment, enchantmentStorageMeta.getStoredEnchants().get(enchantment));
+            enchantmentStorageMeta.removeStoredEnchant(enchantment);
+
+            if(enchantmentStorageMeta.getStoredEnchants().isEmpty()) {
+                event.getWhoClicked().setItemOnCursor(new ItemStack(Material.AIR));
+            }
         }
     }
 }
